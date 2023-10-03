449f7ddee9c05bb75eafb9c4df99587961c3e3ea
Bhuvanesh_babu_Sk <bhuvaneshdev@gmail.com>
2023-10-03 11:45:32 +0530

    initial push project creation

---
+++ heyalle/heyalle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  heyalle
+//
+//  Created by Bhuvanesh Babu on 03/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
